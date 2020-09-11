06832d3b54434d937675664e6f0f958e8a8b289f
Uellington Damasceno <uellington99@gmail.com>
2020-09-10 21:03:48 -0300

    First commit!

---
+++ src/main/java/org/iot/soft/activator/Activator.java
@@ -0,0 +1,17 @@
+package org.iot.soft.activator;
+
+/**
+ * 
+ * @author Uellington Damasceno
+ */
+public class Activator implements IActivator {
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void stop() {
+    }
+
+}
---
+++ src/main/java/org/iot/soft/activator/IActivator.java
@@ -0,0 +1,13 @@
+package org.iot.soft.activator;
+
+/**
+ * 
+ * @author Uellington Damasceno
+ */
+public interface IActivator {
+    
+    public void start();
+    
+    public void stop();
+    
+}
