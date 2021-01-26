44df86fce71c4f2c3225f72a837afdf64f3bd660
Uellington Damasceno <uellington99@gmail.com>
2021-01-26 15:44:39 -0300

    cfg file properties updated

--- src/main/java/dlt/load/monitor/model/LoadMonitor.java
+++ src/main/java/dlt/load/monitor/model/LoadMonitor.java
@@ -2,9 +2,6 @@ package dlt.load.monitor.model;
 
 //import com.sun.management.OperatingSystemMXBean;
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -12,13 +9,12 @@ import java.util.concurrent.TimeUnit;
  */
 public class LoadMonitor implements Runnable {
 
-    private final ScheduledExecutorService executor;
-//    private OperatingSystemMXBean so;
     private Processor processor;
-
-    public LoadMonitor() {
-        this.executor = Executors.newSingleThreadScheduledExecutor();
-//        this.so = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+    private int samplingInterval, sampling;
+    
+    public LoadMonitor(int samplingInterval, int sampling) {
+        this.samplingInterval = samplingInterval;
+        this.sampling = sampling;
     }
 
     public void setProcessor(Processor processor) {
@@ -29,25 +25,13 @@ public class LoadMonitor implements Runnable {
     }
 
     public void start() {
-//        this.executor.scheduleAtFixedRate(this, 0, 5, TimeUnit.SECONDS);
     }
 
     public void stop() {
-        if (this.executor != null) {
-            this.executor.shutdown();
-            try {
-                this.executor.awaitTermination(3, TimeUnit.SECONDS);
-            } catch (InterruptedException ex) {
-                this.executor.shutdownNow();
-            }
-        }
     }
 
     @Override
     public void run() {
-//        double cpu = this.so.getSystemCpuLoad();
-//        long mem = this.so.getFreePhysicalMemorySize();
-
         double cpu = new Random().nextDouble();
         long mem = new Random().nextLong();
         this.processor.updateBrokerStatus(cpu, mem);
--- src/main/java/dlt/load/monitor/model/Processor.java
+++ src/main/java/dlt/load/monitor/model/Processor.java
@@ -8,20 +8,28 @@ import java.util.Random;
  * @author Uellington Damasceno
  * @version 0.0.1
  */
-public class Processor implements IProcessor<BrokerStatus>{
-    
+public class Processor implements IProcessor<BrokerStatus> {
+
     private BrokerStatus lastValueCalculated;
-    
-    public Processor(){
+    private LedgerConnector connector;
+    private int loadLimit;
+
+    public Processor(int loadLimit) {
         this.lastValueCalculated = new BrokerStatus();
+        this.loadLimit = loadLimit;
+    }
+
+    public void setConnector(LedgerConnector connector){
+        this.connector = connector;
     }
     
-    protected void updateBrokerStatus(double cpu, long mem){
-         this.lastValueCalculated.setFull(new Random().nextBoolean());
+    protected void updateBrokerStatus(double cpu, long mem) {
+        this.lastValueCalculated.setFull(new Random().nextBoolean());
     }
-    
+
     @Override
     public BrokerStatus getLastFitness() {
+        this.updateBrokerStatus(0, 0);
         return this.lastValueCalculated;
-    }    
+    }
 }
