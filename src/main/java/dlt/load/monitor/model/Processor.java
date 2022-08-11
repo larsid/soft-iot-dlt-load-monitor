805ecb5c09c7d5c0022891a8532c7efaaf49316f
AllanCapistrano <allan.capistrano3@gmail.com>
2022-08-11 15:29:58 -0300

    Style: formatting code

--- src/main/java/dlt/load/monitor/model/Processor.java
+++ src/main/java/dlt/load/monitor/model/Processor.java
@@ -7,49 +7,69 @@ package dlt.load.monitor.model;
  */
 public class Processor {
 
-    private final int loadLimit;
-    private LedgerConnector connector;
-    private long lastSentLbEntry;
-    private boolean flag;
-    private long lbEntryTimeout;
+  private final int loadLimit;
+  private LedgerConnector connector;
+  private long lastSentLbEntry;
+  private boolean flag;
+  private long lbEntryTimeout;
 
-    public Processor(int loadLimit) {
-        this.loadLimit = loadLimit;
-    }
-
-    protected void updateBrokerStatus(Integer qtdDevices) throws InterruptedException {
-        boolean lbEntry = (qtdDevices >= loadLimit);
-        boolean available = ((qtdDevices + 1) < loadLimit);
-        
-        System.out.println("Amount of devices: "+ qtdDevices + " | Need balancing? "+ lbEntry);
-
-        if(lbEntry) { //Se o gateway está sobrecarregado.
-          if(flag || System.currentTimeMillis() > this.lastSentLbEntry + (lbEntryTimeout * 1000)) {
-            // Salva o tempo da última vez que enviou uma transação do tipo LB_ENTRY.
-            this.lastSentLbEntry = System.currentTimeMillis();
-            this.flag = false;
-            
-            this.connector.put(qtdDevices, true, qtdDevices.longValue(), qtdDevices.longValue(),available);
-          }
-        } else {
-          this.connector.put(qtdDevices, false, qtdDevices.longValue(), qtdDevices.longValue(),available);
-          this.flag = true;
-        }
-    }
-
-    public void setConnector(LedgerConnector connector) {
-      this.connector = connector;
+  public Processor(int loadLimit) {
+    this.loadLimit = loadLimit;
   }
 
-    public void setLastSentLbEntry(long lastSentLbEntry) {
-      this.lastSentLbEntry = lastSentLbEntry;
-    }
+  protected void updateBrokerStatus(Integer qtdDevices)
+    throws InterruptedException {
+    boolean lbEntry = (qtdDevices >= loadLimit);
+    boolean available = ((qtdDevices + 1) < loadLimit);
 
-    public void setFlag(boolean flag) {
-      this.flag = flag;
-    }
+    System.out.println(
+      "Amount of devices: " + qtdDevices + " | Need balancing? " + lbEntry
+    );
 
-    public void setLbEntryTimeout(long lbEntryTimeout) {
-      this.lbEntryTimeout = lbEntryTimeout;
+    if (lbEntry) { //Se o gateway está sobrecarregado.
+      if (
+        flag ||
+        System.currentTimeMillis() >
+        this.lastSentLbEntry +
+        (lbEntryTimeout * 1000)
+      ) {
+        // Salva o tempo da última vez que enviou uma transação do tipo LB_ENTRY.
+        this.lastSentLbEntry = System.currentTimeMillis();
+        this.flag = false;
+
+        this.connector.put(
+            qtdDevices,
+            true,
+            qtdDevices.longValue(),
+            qtdDevices.longValue(),
+            available
+          );
+      }
+    } else {
+      this.connector.put(
+          qtdDevices,
+          false,
+          qtdDevices.longValue(),
+          qtdDevices.longValue(),
+          available
+        );
+      this.flag = true;
     }
+  }
+
+  public void setConnector(LedgerConnector connector) {
+    this.connector = connector;
+  }
+
+  public void setLastSentLbEntry(long lastSentLbEntry) {
+    this.lastSentLbEntry = lastSentLbEntry;
+  }
+
+  public void setFlag(boolean flag) {
+    this.flag = flag;
+  }
+
+  public void setLbEntryTimeout(long lbEntryTimeout) {
+    this.lbEntryTimeout = lbEntryTimeout;
+  }
 }
