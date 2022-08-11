40c87cbb1bf28b326d9929619aa53c25985333c0
AllanCapistrano <allan.capistrano3@gmail.com>
2022-08-11 15:29:24 -0300

    Feat: add LB_ENTRY timeout

--- src/main/java/dlt/load/monitor/model/Processor.java
+++ src/main/java/dlt/load/monitor/model/Processor.java
@@ -2,27 +2,54 @@ package dlt.load.monitor.model;
 
 /**
  *
- * @author Antonio Crispim, Uellington Damasceno
- * @version 0.0.1
+ * @author Allan Capistrano, Antonio Crispim, Uellington Damasceno
+ * @version 0.0.2
  */
 public class Processor {
 
     private final int loadLimit;
     private LedgerConnector connector;
+    private long lastSentLbEntry;
+    private boolean flag;
+    private long lbEntryTimeout;
 
     public Processor(int loadLimit) {
         this.loadLimit = loadLimit;
     }
 
-    public void setConnector(LedgerConnector connector) {
-        this.connector = connector;
-    }
-
     protected void updateBrokerStatus(Integer qtdDevices) throws InterruptedException {
         boolean lbEntry = (qtdDevices >= loadLimit);
-        System.out.println("Carga: "+ qtdDevices + " Load: "+ lbEntry);
         boolean available = ((qtdDevices + 1) < loadLimit);
-        	
-        this.connector.put(qtdDevices, lbEntry, qtdDevices.longValue(), qtdDevices.longValue(),available);
+        
+        System.out.println("Amount of devices: "+ qtdDevices + " | Need balancing? "+ lbEntry);
+
+        if(lbEntry) { //Se o gateway está sobrecarregado.
+          if(flag || System.currentTimeMillis() > this.lastSentLbEntry + (lbEntryTimeout * 1000)) {
+            // Salva o tempo da última vez que enviou uma transação do tipo LB_ENTRY.
+            this.lastSentLbEntry = System.currentTimeMillis();
+            this.flag = false;
+            
+            this.connector.put(qtdDevices, true, qtdDevices.longValue(), qtdDevices.longValue(),available);
+          }
+        } else {
+          this.connector.put(qtdDevices, false, qtdDevices.longValue(), qtdDevices.longValue(),available);
+          this.flag = true;
+        }
+    }
+
+    public void setConnector(LedgerConnector connector) {
+      this.connector = connector;
+  }
+
+    public void setLastSentLbEntry(long lastSentLbEntry) {
+      this.lastSentLbEntry = lastSentLbEntry;
+    }
+
+    public void setFlag(boolean flag) {
+      this.flag = flag;
+    }
+
+    public void setLbEntryTimeout(long lbEntryTimeout) {
+      this.lbEntryTimeout = lbEntryTimeout;
     }
 }
